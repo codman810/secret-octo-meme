f2339e0cd91b75fb16e2a387c58549525192aa12
Cody Phrampus <cphrampu@purdue.edu>
2014-02-01 14:03:17 -0500

    fade score into multiple functions

--- score/src/score.java
+++ score/src/score.java
@@ -5,49 +5,37 @@ public class score{
 	private static int total_f;
 	private static int friends;
 	private static int followers;
-	private static double dbl_twitter_score;
-	private static double dbl_fb_score;
-	private static int twitter_score;
-	private static int fb_score;
+	private static double twitter_score;
+	private static double fb_score;
 	private static int num_rtf;
 	private static int num_like;
 	public score(){}
-	private static void set_total(){
+	private static void set_total(int friends, int followers){
 		total_f=followers+friends;
 	}
-	private static void set_twitter_weight(){
+	private static double set_twitter_weight(int followers, int total_f){
 		twitter_weight = (double)(followers)/(double)(total_f);
 		twitter_weight *= 100;
+		return twitter_weight;
 	}
-	private static void calc_score_twitter(){
-		dbl_twitter_score = (double)num_rtf/(double)followers;
-		dbl_twitter_score *= constant;
-		dbl_twitter_score *= twitter_weight;
-		twitter_score = (int)(dbl_twitter_score+0.5);
+	private static int calc_score_twitter(int num_rtf, int followers, double twitter_weight){
+		twitter_score = (double)num_rtf/(double)followers;
+		twitter_score *= constant;
+		twitter_score *= twitter_weight;
+		return (int)(twitter_score+0.5);
 	}
-	private static void set_fb_weight(){
+	private static double set_fb_weight(int friends, int total_f){
 		facebook_weight = (double)(friends)/(double)(total_f);
 		facebook_weight *= 100;
+		return facebook_weight;
 	}
-	private static void calc_score_fb(){
-		dbl_fb_score = (double)num_like/(double)friends;
-		dbl_fb_score *= constant;
-		dbl_fb_score *= facebook_weight;
-		fb_score = (int)(dbl_fb_score+0.5);
-	}
-	public static void get_score(int numt, int tf, int numl, int fbf){
-		num_rtf = numt;
-		followers = tf;
-		num_like = numl;
-		friends = fbf;
-		set_total();
-		set_twitter_weight();
-		set_fb_weight();
-		calc_score_twitter();
-		calc_score_fb();
-		StdOut.println("Twitter score: " + twitter_score + " Facebook score: " + fb_score);
+	private static int calc_score_fb(int num_like, int friends, double facebook_weight){
+		fb_score = (double)num_like/(double)friends;
+		fb_score *= constant;
+		fb_score *= facebook_weight;
+		return (int)(fb_score+0.5);
 	}
 	public static void main(String[] args){
-		get_score(StdIn.readInt(),StdIn.readInt(),StdIn.readInt(),StdIn.readInt());
+		
 	}
 }
